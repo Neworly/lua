014dac1f8ef520f77da89356758e8f4f5eaee256
Marling <55161172+Neworly@users.noreply.github.com>
2024-07-07 15:21:40 +0200

    keymapcomp.lua

--- nvim-utils/src.lua
+++ nvim-utils/src.lua
@@ -1,188 +1,142 @@
+local settings = {
+  lang = {
+    c = {
+      GCC = {
+        options = '-Wall -pedantic-errors -fsanitize=address -o',
+        alias = 'gcc',
+      },
+    },
 
-local SOURCE = ":source"
+    lua = {
+      comp = 'source',
+    },
+  },
+  format = {
+    EXCLUDED = -1,
+  },
+}
 
-local FILE_NAME = "main"
-local EFILE_NAME = "lowmain"
+local KEYMAP_COMPILER = 'cr'
 
-local GCC_FLAGS = "-Wall -o"
-local GCC_COMPILE = ":!gcc"
+function get_file_format(filename)
+  local file_format = filename:reverse()
 
-local CARGO_COMPILE = ":!cargo run"
-local CARGO_DEBUG = ":!cargo debug"
-
-local CASSEMBLER_FLAGS = "-S -fverbose-asm"
-
-local ASM_COMPILER = ":!as"
-local ASM_FLAGS = "-o"
-local ASM_LINKER = "ld"
-
-local KEYMAP_COMPILER = "cr"
-local KEYMAP_DEBUG = "cd"
-local KEYMAP_ASSEMBLE = "ca"
-
-local function sformat(...)
-		return (""):rep(#{ ... }+1, "%s "):format(...)
-end
-
-local function NOT_SUPPORTED_LANG(ft)
-		return sformat(ft, "is not a supported language")
-end
-
-local function INVALID_DEBUG_OPTION(ft)
-		return sformat(ft, "encountered an invalid debug option by 'this executive'")
-end
-
-local function INVALID_ASM_OPTION(ft)
-		return sformat(ft, "an asssembler were not supported")
-end
-
-
-local EXIT_SUCCESS, EXIT_FAILED = 1, 0
-local OKEY, FAILED = true, false
-
-local function lang_opts()
-		return {
-				c = {
-            run = sformat(GCC_COMPILE, GCC_FLAGS, FILE_NAME, FILE_NAME..".c"),
-            asm = sformat(GCC_COMPILE, CASSEMBLER_FLAGS, FILE_NAME..".c"),
-            linked = FILE_NAME
-        }, s = {
-            run = sformat(sformat(ASM_COMPILER, ASM_FLAGS, FILE_NAME..".o", FILE_NAME..".s"), ";", ASM_LINKER, ASM_FLAGS, EFILE_NAME, FILE_NAME..".o"),
-            linked = EFILE_NAME
-        },
-				lua = SOURCE,
-				rust = {
-						run = CARGO_COMPILE,
-						debug = CARGO_DEBUG
-				}
-		}
-end
-
-local function active_file_ex()
-		local path = vim.fn.expand("%")
-		local path_len = path:len()
-		local TERMINATOR = ("."):byte(1, 1)
-		local buffer = ""
-
-		for cursor = path_len, 1, -1 do
-				local byte = path:byte(cursor);
-				if byte == TERMINATOR then
-						if buffer:len() == 0 then
-							goto continue
-						end
-						buffer = buffer:reverse()
-						return buffer
-				end
-		::continue::
-				buffer = buffer .. string.char(byte)
-		end
-
-		return EXIT_FAILED
-end
-
-local function comp_mode(flag)
-		local opts = lang_opts()
-		local file_type = active_file_ex();
-		if not file_type then
-			return EXIT_FAILED
-		end
-
-		local directive = opts[file_type]
-		if not directive then
-				print(NOT_SUPPORTED_LANG(file_type))
-				return EXIT_FAILED
-		end
-
-		local function supported_run()
-				if type(directive) == type({}) then
-            if directive.run then
-                return OKEY
-            end
-						print(INVALID_DEBUG_OPTION(file_type))
-          	return FAILED
-				end
-				return OKEY
-		end
-
-		local function supported_debug()
-				if type(directive) == "table" and not directive.run then
-						print(INVALID_DEBUG_OPTION(file_type))
-						return FAILED
-				end
-				return OKEY
-		end
-
-
-		local function supported_assemble()
-				if type(directive) == "table" and not directive.asm then
-						print(INVALID_ASM_OPTION(file_type))
-						return FAILED
-				end
-				return OKEY
-		end
-
-
-		local function compile()
-				local status = supported_run()
-				if not status then
-					  return
-				end
-
-        local function call_exe(com)
-				    vim.api.nvim_command(com)
-            if type(directive) == "table" and directive.linked then
-                vim.api.nvim_command(string.format("!./%s", directive.linked))
-            end
+  local s, e =
+    0, (function()
+      local ebyte = string.byte '.'
+      for i = 1, file_format:len(), 1 do
+        if file_format:byte(i) == ebyte then
+          return i > 1 and i - 1 or i
         end
+      end
+      return -1
+    end)()
 
-				if type(directive) == "table" then
-            call_exe(directive.run)
-					  return
-				end
-        call_exe(directive)
-		end
+  if settings.format.EXCLUDED == e then
+    error(file_format:reverse() .. ' !! has no format')
+    return settings.format.EXCLUDED
+  end
 
-		local function debug()
-				local status = supported_debug()
-				if not status then
-					return
-				end
+  file_format = file_format:sub(s, e):reverse()
+  return 0, file_format
+end
 
-				vim.api.nvim_command(directive.debug)
-		end
+function auth_format(filename)
+  local retval, format = get_file_format(filename)
 
-	  local function assemble()
-				local status = supported_assemble()
-				if not status then
-					return
-				end
-				vim.api.nvim_command(directive.asm)
-		end
+  if settings.format.EXCLUDED == retval then
+    return false
+  end
 
-    if flag == "debug" then
-        debug()
-        return
+  local t = settings.lang[format]
+  if not t then
+    return false
+  end
+
+  return true, t
+end
+
+function compacker()
+  local owner = io.open('pack.md', 'r')
+  if not owner then
+    return false
+  end
+
+  local lines = {}
+
+  for line in owner:lines '*l' do
+    line = line:gsub(' ', '')
+    if line:len() == 0 then
+    else
+      if auth_format(line) then
+        table.insert(lines, line)
+      end
+    end
+  end
+
+  if #lines == 0 then
+    return false
+  end
+
+  return true, lines
+end
+
+function compile(filename, lang)
+  if type(lang) ~= 'table' then
+    return
+  end
+
+  if lang.source then
+    vim.api.nvim_command(string.format('!source %s', filename))
+  elseif lang.GCC then
+    local bin = {}
+
+    local commexe, commrun
+    do
+      local alias, opt = lang.GCC.alias, lang.GCC.options
+      local prefix = '!' .. alias .. ' '
+      local this = filename:gsub('.c', '')
+      table.insert(bin, this)
+      local suffix = string.format(' %s %s', this, filename)
+      commexe = prefix .. opt .. suffix
+      commrun = '!./' .. this
+    end
+    vim.api.nvim_command(commexe)
+    vim.api.nvim_command(commrun)
+  end
+
+  return true
+end
+
+function file_in_focus()
+  local filename = vim.fn.expand '%'
+  local retval, t = auth_format(filename)
+  if not retval then
+    return
+  end
+  compile(filename, t)
+end
+
+function run()
+  local retval, pack = compacker()
+  if not retval then
+    file_in_focus()
+    return
+  end
+
+  local files = {}
+  for _, v in pairs(pack) do
+    local _, t = auth_format(v)
+    if not _ then
+      return
     end
 
-    if flag == "assemble" then
-        assemble()
-        return
+    if compile(v, t) then
+      table.insert(files, v)
     end
-    compile()
-end
+  end
 
-local function run()
-    comp_mode("run")
-end
-
-local function recompile()
-    comp_mode("debug")
-end
-
-local function asm()
-    comp_mode("assemble")
+  collectgarbage 'collect'
 end
 
 vim.keymap.set('n', KEYMAP_COMPILER, run)
-vim.keymap.set('n', KEYMAP_DEBUG, recompile)
-vim.keymap.set('n', KEYMAP_ASSEMBLE, asm)
